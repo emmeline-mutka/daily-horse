1a5048173287e1fe798150c04fce010de00288dd
Emmeline <emmeline_mutka@hotmail.com>
2022-05-17 14:45:34 +0200

    Added login and create account screens

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import * as firebase from 'firebase/app';
-import firebaseConfig from './services/firebase';
+import { StyleSheet, Text, View } from "react-native";
+import * as firebase from "firebase/app";
+import firebaseConfig from "./services/firebase";
+import { useFonts } from "expo-font";
+import LoginScreen from "./screens/LoginScreen";
+import CreateAccountScreen from "./screens/CreateAccountScreen";
 
 firebase.initializeApp(firebaseConfig);
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'LifeSavers-Regular': require("./assets/fonts/life-savers/LifeSavers-Regular.ttf"),
+    'LifeSavers-Bold': require("./assets/fonts/life-savers/LifeSavers-Bold.ttf"),
+    'LifeSavers-ExtraBold': require("./assets/fonts/life-savers/LifeSavers-ExtraBold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CreateAccountScreen />
     </View>
   );
 }
@@ -17,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
